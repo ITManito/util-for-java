95cc166bbbf198d6794668988b5cdeeca55a2ef9
angel <18232466045@163.com>
2019-12-24 09:41:58 +0800

    commit util for java -angel-2019年12月24日09:41:50

---
+++ src/main/java/model/Model.java
@@ -0,0 +1,14 @@
+package model;
+
+import com.alibaba.excel.metadata.BaseRowModel;
+
+/**
+ * @author rmm
+ * @version 1.0.0
+ * @since 2019/10/4 17:09
+ */
+public class Model extends BaseRowModel {
+    private String name;
+    private String age;
+    private String phphoneNum;
+}
---
+++ src/test/java/util/EasyExcelUtilTest.java
@@ -0,0 +1,29 @@
+package util;
+
+import demo.EasyExcelUtil;
+import model.Model;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * @author rmm
+ * @version 1.0.0
+ * @since 2019/10/4 16:58
+ */
+public class EasyExcelUtilTest {
+    @Test
+    public void test_parse_excel() {
+        Resource resource = new ClassPathResource("mm.xlsx");
+        try {
+            List<Model> list = (List<Model>) EasyExcelUtil.readExcelByModelSheetName(resource.getFile(), "Sheet1", Model.class);
+            System.out.println(list);
+//            Assert.assertEquals(list.size(),10000);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
